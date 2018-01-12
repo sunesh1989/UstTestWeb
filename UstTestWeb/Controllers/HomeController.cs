10edaffff1cba0ee7349ac000c80798d84f53258
Sunesh <Sunesh.Padinhantan@ust-global.com>
2018-01-12 14:35:35 +0530

    First commit

---
+++ UstTestWeb/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UstTestWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Queue()
+        {
+            return View();
+        }
+        public ActionResult RSSFeed()
+        {
+
+
+            HttpResponseMessage response = null;
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/xml"));
+                try
+                {
+                     response = client.GetAsync("https://platform.boomi.com/account/trainingvishnurajnr-48CRQA/feed/category/alert/rss-2.0").Result;
+                }
+                catch(Exception e)
+                {
+                    throw e;
+                    // TODO: Log
+                }
+            }
+            ViewData["test"] = "ss";
+
+            return View();
+        }        
+    }
+}
---
+++ UstTestWeb/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="UstTestWeb.MvcApplication" Language="C#" %>

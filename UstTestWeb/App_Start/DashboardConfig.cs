9f13b8f98135a7a33c91fb420f8df51c552f5376
Sunesh <Sunesh.Padinhantan@ust-global.com>
2018-01-15 12:27:26 +0530

    Added Queue and RssFeed updated mthod definitions

---
+++ UstTestWeb/App_Start/DashboardConfig.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.ServiceBus;
+using Microsoft.ServiceBus.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace UstTestWeb.App_Start
+{
+    public static class DashboardConfig
+    {
+        public static string keyName = string.Empty;
+        public static string accessKey = string.Empty;
+        public static string baseAddress = string.Empty;
+        public static string queueName = string.Empty;       
+        public static void Config()
+        {
+            keyName = ConfigurationManager.AppSettings["keyName"];
+            accessKey = ConfigurationManager.AppSettings["accessKey"];
+            baseAddress = ConfigurationManager.AppSettings["baseAddress"];
+            queueName = ConfigurationManager.AppSettings["queueName"];
+          
+        }
+    }
+}
--- UstTestWeb/Global.asax.cs
+++ UstTestWeb/Global.asax.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using UstTestWeb.App_Start;
 
 namespace UstTestWeb
 {
@@ -16,6 +17,7 @@ namespace UstTestWeb
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            DashboardConfig.Config();
         }
     }
 }
